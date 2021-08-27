c5ca55f505cdd564f7731bd02edf2bc494f38ea9
Tushar-Maity <tusharmaity989@gmail.com>
2021-08-27 22:51:14 +0530

    todoform basic functions added

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react'
 
-const TodoForm = () => {
+const TodoForm = (props) => {
     const [input, setInput] = useState('');
 
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // props.onSubmit({
+        //     id: Math.floor(Math.random() * 10000);
+        // });
+
+        setInput('');
+    }
+
     return (
-        <form className="todo-form">
+        <form className="todo-form" onSubmit={handleSubmit}>
             <input 
                 type="text"
                 placeholder="Add a ToDo"
                 value={input}
                 name="text"
                 className="todo-input"
+                onChange={handleChange}
             />
             <button className="todo-button">Add a ToDo</button>
         </form>
