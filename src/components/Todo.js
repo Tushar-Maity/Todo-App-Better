77e7ec344535fd2b6335687286a8ea0b9668716e
Tushar-Maity <tusharmaity989@gmail.com>
2021-08-28 16:34:54 +0530

    removeTodo function is added to remove the todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import TodoForm from './TodoForm'
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
 
-const Todo = ({ todos, completeTodo }) => {
+const Todo = ({ todos, completeTodo, removeTodo }) => {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
@@ -20,8 +20,14 @@ const Todo = ({ todos, completeTodo }) => {
                 {todo.text}
             </div>
             <div className="icons">
-                <RiCloseCircleLine />
-                <TiEdit />
+                <RiCloseCircleLine 
+                    onClick={() => removeTodo(todo.id)}
+                    className='delete-icon'
+                />
+                <TiEdit 
+                    onClick={() => setEdit({ id: todo.id, value: todo.text })}
+                    className='edit-icon'
+                />
             </div>
         </div>
     ))
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -16,6 +16,12 @@ const TodoList = () => {
         console.log(todo,...todos);
     }
 
+    const removeTodo = id => {
+        const removedArr = [...todos].filter(todo => todo.id !== id);
+    
+        setTodos(removedArr);
+    };
+
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
             if(todo.id === id) {
@@ -29,7 +35,7 @@ const TodoList = () => {
         <div>
             <h1>Hey What's the plan for today??</h1>
             <TodoForm onSubmit={addTodo}/>
-            <Todo todos={todos} completeTodo={completeTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}/>
         </div>
     )
 }
